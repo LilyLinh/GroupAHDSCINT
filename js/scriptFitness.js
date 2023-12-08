7c2ad1fd399938367d5c59f78d3b2531202a189a
Adrian Lucas <148779777+itsadrianlucas@users.noreply.github.com>
2023-12-08 15:32:12 +0000

    Pilot Project - Test 1
    Uploading and Merging files created by Adrian and Glen

--- js/scriptFitness.js
+++ js/scriptFitness.js
@@ -1,5 +1,5 @@
 
-// JavaScript Function for Bmi calculator
+// JavaScript Function for Bmi calculator - Glen
 
 function calculateBMI() {
     
@@ -41,3 +41,74 @@ function calculateBMI() {
     document.getElementById('message').innerHTML = messages[index];
 }
 
+
+// JavaScript - Adrian
+
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#nameUser");
+const emailInput = document.querySelector("#emailUser");
+const subjectSelected = document.querySelector("#subjectUser");
+const checkBox = document.querySelector("#checkboxTerms");
+
+console.log(form);
+console.log(nameInput);
+console.log(emailInput);
+console.log(subjectSelected);
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    //Verify if name is empty
+    if (nameInput.value === ""){
+        alert("Please fill up your Name");
+        return;
+    }
+
+    //Verify if name is empty and is valid
+    if (emailInput.value === "" || !emailValidation(emailInput.value)){
+        alert("Please fill up your Email");
+        return;
+    }
+
+    //Verify if options is selected
+    if (subjectUser.value === ""){
+        alert("Please fill up the subject");
+        return;
+    }
+
+    if (checkBox.checked){
+        console.log("User OK");
+    }else{
+        alert("Please check the box to agree to receive comunications");
+        return;
+    }
+
+    //if all the fields are filled up, submit form
+    form.submit();
+})
+
+
+function emailValidation(email){
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.test(email)){
+        return true;
+    }
+
+    return false;
+
+}
+
+
+function spanMsgDayTime(){
+    if (new Date().getHours() < 12) {
+        document.getElementById("span-msg-hourday").innerText = "Good Morning";
+    } else if (new Date().getHours() >= 12 && new Date().getHours() <= 18){
+        document.getElementById("span-msg-hourday").innerHTML = "Good Afternoon";
+    }
+    else{
+        document.getElementById("span-msg-hourday").innerHTML = "Good Evening";
+    }
+}
